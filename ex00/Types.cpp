082888b2d01bcf5f66fd8fe4197f903651956dc0
tpawson <tpawson@student.42adel.org.au>
2024-04-12 15:59:31 +0930

    types.cpp added

--- ex00/Types.cpp
+++ ex00/Types.cpp
@@ -1,5 +1,16 @@
 #include "ScalarConverter.hpp"
 
+static int isOddCase(const std::string src)
+{
+	if (src == "nan" || src == "nanf" || src == "+inf" || src == "+inff" || src == "-inf" || src == "-inff")
+		return 1;
+	return 0;
+}
+
+static int isChar(const std::string src, int len)
+{
+	
+}
 int getType(std::string &src, int &len)
 {
 	int f_char = src.find('f'), dot_char = src.find('.');
