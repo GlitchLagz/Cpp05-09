2e85f29517976cb758fbcb3df9463385b1f5823a
tpawson <tpawson@student.42adel.org.au>
2024-04-12 12:31:10 +0930

    added

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -17,3 +17,52 @@ ScalarConverter::ScalarConverter()
 {
 	return ;
 }
+
+void conInt(const std::string &src)
+{
+	long cpy = std::atol(src.c_str());
+	
+	std::cout << "char: ";
+	if (cpy > 127 || cpy < 0)
+		std::cout << "impossible" << std::endl;
+	else{
+		if (!isprint(cpy))
+			std::cout << "ERR: Cant display this char" << std::endl;
+		else
+			std::cout << "'" << static_cast<char>(cpy) << "'" << std::endl;
+	}
+	std::cout << "int: ";
+	if (cpy < MIN_INT || cpy > MAX_INT)
+		std::cout << "impossible" << std::endl;
+	else
+		std::cout << static_cast<int>(cpy) << std::endl;
+	std::cout << "float: " << static_cast<float>(cpy) << ".0f" << std::endl;
+	std::cout << "double: " << static_cast<double>(cpy) << ".0" << std::endl;
+}
+
+void ScalarConverter::convert(std::string src)
+{
+	int len = src.length();
+	int type = getType(src, len);
+
+	switch(type)
+	{
+		case INT:
+			conInt(src);
+			break;
+		case DUB:
+			conDouble(src);
+			break;
+		case FLO:
+			conFloat(src);
+			break;
+		case CHR:
+			conChar(src, len);
+			break;
+		case SPE:
+			printSpecial(src);
+		default:
+			std::cout << "ERR: Invalid input" << std::endl;
+			break;
+	}
+}
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -1,6 +1,23 @@
 #pragma once
 # include <iostream>
 # include <string>
+# include <limits>
+# include <cstdlib>
+# include <cctype>
+
+# define MAX_DOUBLE std::numeric_limits<double>::max()
+# define MIN_DOUBLE std::numeric_limits<double>::min()
+# define MAX_FLOAT std::numeric_limits<float>::max()
+# define MIN_FLOAT std::numeric_limits<float>::min()
+# define MAX_INT std::numeric_limits<int>::max()
+# define MIN_INT std::numeric_limits<int>::min()
+# define INV -1
+# define INT 0
+# define DUB 1
+# define FLO 2
+# define CHR 3
+# define SPE 4
+
 
 class ScalarConverter
 {
@@ -10,13 +27,13 @@ class ScalarConverter
 		ScalarConverter(ScalarConverter const &src);
 		~ScalarConverter();
 
-		static void fromInt();
-		static void fromChar();
-		static void fromFloat();
-		static void fromDouble();
-		
-
 	public:
 		static void convert(std::string src);
 
 };
+	void conInt(const std::string &src);
+	void conChar(const std::string &src, int &len);
+	void conFloat(const std::string &src);
+	void conDouble(const std::string &src);
+	void printSpecial(const std::string &src);
+	int getType(const std::string &src, int &len);
