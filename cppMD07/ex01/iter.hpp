46166f2814c3f1231089586828480cab8d8f8bdf
tpawson <tpawson@student.42adel.org.au>
2024-04-24 12:08:20 +0930

    fixing ex01

--- cppMD07/ex01/iter.hpp
+++ cppMD07/ex01/iter.hpp
@@ -1,2 +1,11 @@
 #pragma once
 
+#include <string>
+#include <iostream>
+
+template<typename T, typename T_funct>
+void iter(T *array, int len, T_funct f)
+{
+	for (int i = 0; i < len; i++)
+		f(array[i]);
+}
--- cppMD07/ex01/main.cpp
+++ cppMD07/ex01/main.cpp
@@ -1,7 +1,33 @@
 #include "iter.hpp"
 
+void decrement(int const &n)
+{
+	std::cout << n - 1 << std::endl;
+}
+
+void print(std::string const &n)
+{
+	std::cout << n << std::endl;
+}
+
+void to_lower(std::string &src)
+{
+    for(int i = 0; i < src.length(); i++)
+        src[i] = std::tolower(src[i]);
+}
+
 int main(void)
 {
-    
+    int array[] = {4, 2, 6, 2};
+	std::string strings[] = {"ABC", "123", "HELLO WORLD!"};
+	std::cout << std::endl;
+	::iter(strings, 3, print);
+    ::iter(strings, 3, to_lower);
+    std::cout << std::endl;
+    ::iter(strings, 3, print);
+    std::cout << std::endl;
+	::iter(array, 4, decrement);
+	std::cout << std::endl;
+
     return 0;
 }
