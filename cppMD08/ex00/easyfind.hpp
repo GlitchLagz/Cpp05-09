5eebda2a7ea378cd81fa5565e050203b63e09b8c
tpawson <tpawson@student.42adel.org.au>
2024-04-26 17:19:22 +0930

    hpp done

--- cppMD08/ex00/easyfind.hpp
+++ cppMD08/ex00/easyfind.hpp
@@ -1,11 +1,17 @@
 #pragma once
 #include <iostream>
 #include <exception>
+#include <algorithm>
 
 template<typename T>
 typename T::iterator easyfind(T &src, int tofind)
 {
 	typename T::iterator itr;
+
+	itr = std::find(src.begin(), src.end(), n);
+	if (itr == src.end())
+		throw NotFoundExeception();
+	return itr;
 }
 class NotFoundException : public std::exception
 {
--- cppMD08/ex00/main.cpp
+++ cppMD08/ex00/main.cpp
@@ -2,7 +2,7 @@
 
 int main(void)
 {
+	std::vector<int> vec;
 
-	
 	return 0;
 }
