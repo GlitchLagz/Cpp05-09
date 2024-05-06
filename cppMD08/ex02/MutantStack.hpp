2f93ca60ae9289c2ed9191e356ca4d5db2a64ee0
tpawson <tpawson@student.42adel.org.au>
2024-05-06 12:56:30 +0930

    ex02 progress

--- cppMD08/ex02/MutantStack.hpp
+++ cppMD08/ex02/MutantStack.hpp
@@ -1,20 +1,28 @@
 #pragma once
-#include "MutantStack.tpp"
 #include <stack>
 #include <iostream>
+#include "MutantStack.tpp"
 
 template <typename T>
 class MutantStack : public std::stack<T>
 {
-	private:
-
-	protected:
-		
 	public:
 		MutantStack();
 		MutantStack(MutantStack consr &src);
 		MutantStack &operator=(MutantStack const &src);
 		~MutantStack();
 
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator const_reverse_iterator;
 
-}
+		iterator begin();
+		iterator end();
+		const_iterator begin() const;
+		const_iterator end() const;
+		reverse_iterator rbegin();
+		reverse_iterator rend();
+		const_reverse_iterator rbegin() const;
+		const_reverse_iterator rend() const;
+};
